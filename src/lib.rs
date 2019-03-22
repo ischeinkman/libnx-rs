c0862885a9a2bc04d333e5b66aac744c4cbd66d5
XorTroll <xabofdez@outlook.es>
2019-03-22 17:24:54 +0100

    Some changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,7 @@
+#![no_std]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-extern crate core;
 
-pub mod libnx;
-
-pub unsafe fn test()
-{
-    libnx::consoleInit(std::ptr::null_mut());
-    println!("Hello from libnx!");
-    loop
-    {
-        libnx::hidScanInput();
-        let k = libnx::hidKeysDown(libnx::HidControllerID_CONTROLLER_P1_AUTO) as u32;
-        if (k & libnx::HidControllerKeys_KEY_A) != 0
-        {
-            println!("A was pressed!");
-        }
-        else if (k & libnx::HidControllerKeys_KEY_PLUS) != 0
-        {
-            println!("Plus was pressed. Exiting...");
-            libnx::consoleUpdate(std::ptr::null_mut());
-            break;
-        }
-        libnx::consoleUpdate(std::ptr::null_mut());
-    }
-    libnx::consoleUpdate(std::ptr::null_mut());
-}
+pub mod nx;
