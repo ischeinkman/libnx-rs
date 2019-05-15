eb2671500c05703b7f16caaa4eac1ecb3f245a8e
leo60228 <iakornfeld@gmail.com>
2019-05-15 11:01:01 -0400

    Use new handle system for sm

--- src/sm.rs
+++ src/sm.rs
@@ -1,23 +1,12 @@
 use os;
 use sys;
 
-pub fn initialize() -> os::Result<()> {
-    unsafe {
-        let rc = sys::smInitialize();
-        result_final!(rc)
+service!(Handle, sys::smInitialize(), true, sys::smExit(), {
+    pub fn get_service(&self, name: &str) -> os::Result<sys::Service> {
+        unsafe {
+            let mut srv: sys::Service = std::mem::zeroed();
+            let rc = sys::smGetService(&mut srv, name.as_ptr());
+            result_final!(rc, srv)
+        }
     }
-}
-
-pub fn get_service(name: &str) -> os::Result<sys::Service> {
-    unsafe {
-        let mut srv: sys::Service = std::mem::zeroed();
-        let rc = sys::smGetService(&mut srv, name.as_ptr());
-        result_final!(rc, srv)
-    }
-}
-
-pub fn exit() {
-    unsafe {
-        sys::smExit();
-    }
-}
+});
